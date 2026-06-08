9e6476c36dd294b561f1294a87726f185c05f900
Thomas Geri <gerimaulana2455@gmail.com>
2026-06-08 22:54:43 +0700

    Update konfigurasi deployment GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/quizify/', // <--- Tambahkan baris ini sesuai nama repositori GitHub kamu
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
